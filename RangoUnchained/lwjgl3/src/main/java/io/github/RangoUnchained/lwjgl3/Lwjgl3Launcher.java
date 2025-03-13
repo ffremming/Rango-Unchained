c15a7e2dbabad8a01cfcc02301249af196dbbcd2
Elin Arna <elinasi@stud.ntnu.no>
2025-03-13 08:20:22 +0000

    Lagd grunnleggende gameController og ScreenController

--- RangoUnchained/lwjgl3/src/main/java/io/github/RangoUnchained/lwjgl3/Lwjgl3Launcher.java
+++ RangoUnchained/lwjgl3/src/main/java/io/github/RangoUnchained/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,7 @@ package io.github.RangoUnchained.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.github.RangoUnchained.Main;
+import io.github.RangoUnchained.Controllers.GameController;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -12,7 +12,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+        return new Lwjgl3Application(GameController.getInstance(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {

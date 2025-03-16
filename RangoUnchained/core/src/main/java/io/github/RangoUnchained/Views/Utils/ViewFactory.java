7f2e1419d6f0f2bd24a4521ca5ae5fd57bd0431c
Elin Arna <elinasi@stud.ntnu.no>
2025-03-16 13:58:09 +0000

    slått sammen gamecontroller og screen og implementert det sammen med views. Endret på View for mer oversiktlig og effektiv screenChange ved å legge til simple factory.

---
+++ RangoUnchained/core/src/main/java/io/github/RangoUnchained/Views/Utils/ViewFactory.java
@@ -0,0 +1,28 @@
+package io.github.RangoUnchained.Views.Utils;
+
+import com.badlogic.gdx.Screen;
+
+import io.github.RangoUnchained.Views.GameOverView;
+import io.github.RangoUnchained.Views.GamePlayView;
+import io.github.RangoUnchained.Views.MainMenuView;
+import io.github.RangoUnchained.Views.ScoreboardView;
+import io.github.RangoUnchained.Views.SelectLevelView;
+
+public class ViewFactory {
+    public static Screen createView(String type) {
+        switch (type) {
+            case "MAIN_MENU":
+                return new MainMenuView();
+            case "GAMEPLAY":
+                return new GamePlayView();
+            case "SCORE_BOARD":
+                return ScoreboardView.getInstance();
+            case "LEVEL_SCREEN":
+                return new SelectLevelView();
+            case "GAME_OVER":
+                return new GameOverView();
+            default:
+                throw new IllegalArgumentException("Unknown screen type: " + type);
+        }
+    }
+}

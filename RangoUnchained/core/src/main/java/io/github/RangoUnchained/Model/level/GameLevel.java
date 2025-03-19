57aa35fd2cacaea7bc283639a2be35d8a69fd413
Ferdinand Fremming <ferdinf@stud.ntnu.no>
2025-03-19 19:59:10 +0100

    enhancement: finish level model finished basic level model (prototype). includes a system for loading entities (prototype)

--- RangoUnchained/core/src/main/java/io/github/RangoUnchained/Model/level/GameLevel.java
+++ RangoUnchained/core/src/main/java/io/github/RangoUnchained/Model/level/GameLevel.java
@@ -1,12 +1,15 @@
-package io.github.RangoUnchained.level;
+package io.github.RangoUnchained.Model.level;
 
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import java.util.ArrayList;
 
+import io.github.RangoUnchained.Model.Entities.Entity;
+import io.github.RangoUnchained.Model.Factories.EntityFactory;
+
 public class GameLevel {
-    private ArrayList<Object> entities;  // The actual game entities
+    private ArrayList<Entity> entities;  // The actual game entities
     private MetaData metaData;
 
     private GameLevel(){}
@@ -26,22 +29,15 @@ public class GameLevel {
         GameLevel level = json.fromJson(GameLevel.class, file.readString());
 
         // Log level name for testing
-        Gdx.app.log("JSON_testing", "levelName: " + level.metaData.levelName);
-
-
+        Gdx.app.log("JSON_testing", "levelName: " + level.metaData.number);
 
         for (EntityData entityData : level.entitiesData) {
             Gdx.app.log("JSON_testing", "Name: " + entityData.name);
-            Gdx.app.log("JSON_testing", "Position: (" + entityData.position.x + ", " + entityData.position.y + ")");
+            Gdx.app.log("JSON_testing", "Position: (" + entityData.pos.x + ", " + entityData.pos.y + ")");
 
-            //TODO FACTORY
-            // Use the factory to create the actual entity based on the name
-            //Object entity = EntityFactory.createEntity(entityData);
-            //createdEntities.add(entity);
+            Entity entity = EntityFactory.createPlayerEntity(entityData.pos.x, entityData.pos.y, entityData.name);
+            level.entities.add(entity);
         }
-        //TODO set/add after the factory is done
-        //level.entities = createdEntities;
-
         return level;
     }
 
@@ -59,7 +55,7 @@ public class GameLevel {
      * @param entity The entity to add to the level.
      * @return true if the entity was successfully added.
      */
-    public boolean addEntity(Object entity) {
+    public boolean addEntity(Entity entity) {
         entities.add(entity);
         return true;
     }
@@ -69,7 +65,7 @@ public class GameLevel {
      *
      * @return A list of entities currently in the level.
      */
-    public ArrayList<Object> getEntities() {
+    public ArrayList<Entity> getEntities() {
         return entities;
     }
 
@@ -78,11 +74,11 @@ public class GameLevel {
      */
     public static class EntityData {
         public String name;
-        public Position position;
+        public Position pos;
 
         public static class Position {
-            public int x;
-            public int y;
+            public float x;
+            public float y;
         }
     }
 
@@ -96,8 +92,7 @@ public class GameLevel {
      * Class representing the level's metadata (levelName, difficulty, theme, multiplayer).
      */
     public static class MetaData {
-        public String levelName;
-        public int difficulty;
+        public int number;
         public String theme;
         public boolean multiplayer;
     }

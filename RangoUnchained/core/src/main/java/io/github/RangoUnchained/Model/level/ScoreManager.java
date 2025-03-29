7e99661e674ccfca53b3180ed8d4c266028ab67a
Ferdinand Fremming <ferdinf@stud.ntnu.no>
2025-03-29 19:16:51 +0100

    enhancement: contact logic for adding points

--- RangoUnchained/core/src/main/java/io/github/RangoUnchained/Model/level/ScoreManager.java
+++ RangoUnchained/core/src/main/java/io/github/RangoUnchained/Model/level/ScoreManager.java
@@ -1,16 +1,13 @@
 package io.github.RangoUnchained.Model.level;
 
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.ContactImpulse;
-import com.badlogic.gdx.physics.box2d.ContactListener;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.Manifold;
-
+import io.github.RangoUnchained.Controllers.LevelController;
 import io.github.RangoUnchained.Model.Entities.BallEntity;
-import io.github.RangoUnchained.Model.Entities.Entity;
 import io.github.RangoUnchained.Model.Entities.ProjectileEntity;
+import io.github.RangoUnchained.Model.Systems.ContactSystem;
+import io.github.RangoUnchained.Model.Systems.ContactSystem.CollisionEvent;
+import io.github.RangoUnchained.Model.contactListener.ContactStrategy;
 
-public class ScoreManager implements ContactListener{
+public class ScoreManager implements ContactStrategy{
 
     int score = 0;
     public ScoreManager(){
@@ -23,45 +20,21 @@ public class ScoreManager implements ContactListener{
 
     public int getScore(){return score;}
 
-    @Override
-    public void endContact(Contact contact) {
-
-        Fixture fixtureA = contact.getFixtureA();
-        Fixture fixtureB = contact.getFixtureB();
-
-        Object dataA = fixtureA.getBody().getUserData();
-        Object dataB = fixtureB.getBody().getUserData();
-
-        if (dataA == null || dataB == null) return;
-        if (!(dataA instanceof Entity) || !(dataB instanceof Entity)) return;
-
-        Entity entityA = (Entity) dataA;
-        Entity entityB = (Entity) dataB;
-
-
-        // Check ball-player collision
-        if (entityA instanceof BallEntity && entityB instanceof ProjectileEntity) {
-            handleBallProjectileCollision((BallEntity) entityA);
-        } else if (entityA instanceof ProjectileEntity && entityB instanceof BallEntity) {
-            handleBallProjectileCollision((BallEntity) entityB);
-        }
-    }
     
-    private void handleBallProjectileCollision(BallEntity ball){
+    
+    private void handleBallProjectileCollision(CollisionEvent contact){
         //TODO do we want different scores for different balls?
         addScore(1);
     }
 
     @Override
-    public void beginContact(Contact contact) {
+    public void setContactStrategies() {
+         ContactSystem centralContactListener = LevelController.getInstance().getSystem(ContactSystem.class);
+        centralContactListener.subscribe(
+        BallEntity.class, ProjectileEntity.class,
+        this::handleBallProjectileCollision, // For beginContact
+        null);
     }
 
-    @Override
-    public void preSolve(Contact contact, Manifold oldManifold) {
-    }
-
-    @Override
-    public void postSolve(Contact contact, ContactImpulse impulse) {
-    }
 
 }

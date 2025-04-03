5a68f4f3b8d60bdb560b0f8692533c154fb041dc
Ferdinand Fremming <ferdinf@stud.ntnu.no>
2025-04-03 11:54:21 +0200

    fixed checkpoint

--- RangoUnchained/core/src/main/java/io/github/RangoUnchained/Model/level/GameLevel.java
+++ RangoUnchained/core/src/main/java/io/github/RangoUnchained/Model/level/GameLevel.java
@@ -1,31 +1,15 @@
 package io.github.RangoUnchained.Model.level;
 
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.JsonWriter;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.StringWriter;
 import java.util.ArrayList;
 
 import io.github.RangoUnchained.Controllers.LevelController;
 import io.github.RangoUnchained.Model.Components.BodyComponent;
-import io.github.RangoUnchained.Model.Components.HealthComponent;
-import io.github.RangoUnchained.Model.Components.SpriteComponent;
-import io.github.RangoUnchained.Model.Components.StatComponent;
-import io.github.RangoUnchained.Model.Entities.BallEntity;
 import io.github.RangoUnchained.Model.Entities.Entity;
-import io.github.RangoUnchained.Model.Entities.PlayerEntity;
 import io.github.RangoUnchained.Model.Factories.EntityFactory;
 import io.github.RangoUnchained.Model.level.GameLevel.LevelData.EntityData;
-import io.github.RangoUnchained.Views.Utils.Constants;
 
 /**
  * Represents a game level, containing metadata and entities.
@@ -35,7 +19,6 @@ public class GameLevel {
     private ArrayList<Entity> entities = new ArrayList<>();
     private LevelData.MetaData metaData;
     private ArrayList<LevelData.EntityData> entitiesData;
-    private float checkpointCounter = 0;
     public ScoreManager scoreManager = new ScoreManager();
     public Timer timer = new Timer();
     public int levelNumber;
@@ -88,118 +71,14 @@ public class GameLevel {
         spawn(levelData.entitiesData);
     }
 
-    /**
-     * Writes state of entities to json, including backup file to prevent errors due to corrupt files.
-     *
-     * Writes every 3 seconds to files in local memory,
-     * meaning it will not be written to project filepath.
-     *
-     * @param delta the amount of time passed since last call
-     */
-    public void checkpoint(float delta) {
-        // If counter >= 3, write to JSON, else count and continue
-        if (checkpointCounter < 3) {
-            checkpointCounter += delta;
-            return;
-        }
-
-        ArrayList<LevelData.EntityData> entitiesData = new ArrayList<>();
-
-        // Add every entity that is not a player of ball
-        for (EntityData entityData : this.entitiesData) {
-            if (entityData.name.startsWith("Player") || entityData.name.startsWith("Ball")) {
-                continue;
-            }
-            entitiesData.add(entityData);
-        }
-        // Add every ball and player entity
-        for (Entity entity : entities) {
-            if (entity instanceof BallEntity) {
-                entitiesData.add(makeBallEntity(entity));
-            }
-            if (entity instanceof PlayerEntity) {
-                entitiesData.add(makePlayerEntity(entity));
-            }
-        }
-
-        // Set metaData
-        LevelData.MetaData metaData = new LevelData.MetaData();
-        metaData.progress = 1; // Set to on-going
-        metaData.levelnr = levelNumber;
-        metaData.score = scoreManager.getScore();
-        metaData.time = timer.getTime();
-
-        LevelData levelData = new LevelData();
-        levelData.metaData = metaData;
-        levelData.entitiesData = entitiesData;
-
-        // Write levelData to file and backup file.
-        GameFileHandler.getInstance().writeLevelDataToLocalFile(levelData, "levels/checkpoint.json");
-        GameFileHandler.getInstance().writeLevelDataToLocalFile(levelData, "levels/checkpointBackup.json");
-
-        // After writing, reset counter.
-        checkpointCounter = 0;
+    public void checkpoint(float delta){
+        CheckpointHandler.checkPoint(delta, entitiesData, entities, levelNumber, scoreManager.getScore(), timer.getTime());
     }
 
-    /**
-     * Creates an EntityData object from  Entity object
-     *
-     * @param entity Current entity being mapped
-     * @return EntityData object of the corresponding entity argument
-     */
-    public EntityData makeBallEntity(Entity entity) {
-        EntityData entityData = new EntityData();
-        // Fetch needed components
-        BodyComponent body = (BodyComponent) entity.getComponent(BodyComponent.class);
-        StatComponent stat = (StatComponent) entity.getComponent(StatComponent.class);
-        SpriteComponent sprite = (SpriteComponent) entity.getComponent(SpriteComponent.class);
-
-        // Set the name property
-        entityData.name = stat.getTimesPopped() == 0 ? "BallBig" : stat.getTimesPopped() == 1 ? "BallMedium" : "BallSmall";
-
-        // Set the position property
-        EntityData.Position entityPosition = new EntityData.Position();
-        entityPosition.x = sprite.getSprite().getX();
-        entityPosition.y = sprite.getSprite().getY();
-        entityData.position = entityPosition;
-
-        // Set the velocity property
-        entityData.velocity = body.getBody().getLinearVelocity();
-        return entityData;
-    }
-
-    /**
-     * Creates an EntityData object from Entity object
-     *
-     * @param entity Current entity being mapped
-     * @return EntityData object of the corresponding entity argument
-     */
-    public EntityData makePlayerEntity(Entity entity) {
-        EntityData entityData = new EntityData();
-        // Fetch needed components
-        BodyComponent body = (BodyComponent) entity.getComponent(BodyComponent.class);
-        SpriteComponent sprite = (SpriteComponent) entity.getComponent(SpriteComponent.class);
-        HealthComponent health = (HealthComponent) entity.getComponent(HealthComponent.class);
-
-
-        // Set the name property
-        entityData.name = "Player";
-
-        // Set the position property
-        EntityData.Position entityPosition = new EntityData.Position();
-        entityPosition.x = sprite.getSprite().getX();
-        entityPosition.y = 150;
-        entityData.position = entityPosition;
-        entityData.health = health.getHealth();
-
-        return entityData;
-    }
 
     public Timer getTimer() {
         return this.timer;
     }
-
-
 
 
     /**

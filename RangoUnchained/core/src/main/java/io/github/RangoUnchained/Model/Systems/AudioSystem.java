505bef2ebef76add5e5a4339cdf637a0a5212682
Elin Arna <elinasi@stud.ntnu.no>
2025-04-10 16:51:54 +0200

    Fjernet Queue audio component fra input system.

--- RangoUnchained/core/src/main/java/io/github/RangoUnchained/Model/Systems/AudioSystem.java
+++ RangoUnchained/core/src/main/java/io/github/RangoUnchained/Model/Systems/AudioSystem.java
@@ -1,14 +1,12 @@
 package io.github.RangoUnchained.Model.Systems;
 
-
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
-
 
 import java.util.List;
 import java.util.Random;
 
 import io.github.RangoUnchained.Model.Components.AudioComponent;
+import io.github.RangoUnchained.Model.Components.InputComponent;
 import io.github.RangoUnchained.Model.Entities.Entity;
 import io.github.RangoUnchained.Model.Factories.AudioLoader;
 
@@ -23,7 +21,8 @@ public class AudioSystem implements System {
         audioLoader = AudioLoader.getInstance();
     }
 
-    private void playSound (List<Sound> sounds){
+    private void playSound (AudioComponent.ActionType soundKey){
+        List<Sound> sounds = audioLoader.getSounds(soundKey);
         if (sounds != null && !sounds.isEmpty()) {
             sounds.get(new Random().nextInt(sounds.size())).play();
         }
@@ -32,12 +31,20 @@ public class AudioSystem implements System {
     @Override
     public void updateEntity(Entity entity, float delta) {
         AudioComponent audio = (AudioComponent) entity.getComponent(AudioComponent.class);
+        InputComponent input = (InputComponent) entity.getComponent(InputComponent.class);
         AudioComponent.ActionType soundKey;
 
+        if (input.isShoot()){
+            playSound(AudioComponent.ActionType.SHOOT);
+        }
+        if (input.isLeft() || input.isRight()){
+            if (!audio.audioQueue.contains(AudioComponent.ActionType.MOVE)){
+                playSound(AudioComponent.ActionType.MOVE);
+            }
+        }
+
         while ((soundKey = audio.audioQueue.poll()) != null) {
-            Gdx.app.log("Audio", " "+entity + " " + soundKey);
-            List<Sound> sounds = audioLoader.getSounds(soundKey);
-            playSound(sounds);
+            playSound(soundKey);
         }
     }
 
